ee7b03cf6bb3a1d60adaa72f4d0f726431874e03
ziyad77 <method.z@hotmail.fr>
2015-06-25 13:52:37 +0200

    Mise à jour de la nav-bar avec intégration de FOSUserBundle

--- src/Stagia/AppBundle/Menu/MenuBuilder.php
+++ src/Stagia/AppBundle/Menu/MenuBuilder.php
@@ -4,23 +4,31 @@ namespace Stagia\AppBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 class MenuBuilder
 {
     private $factory;
     protected $container;
-    
+    protected $isLoggedIn;
+    protected $securityContext;
+
+
     /**
      * 
      * @param \Knp\Menu\FactoryInterface $factory
      * 
      */
-    public function __construct(FactoryInterface $factory, ContainerInterface $container)
+    public function __construct(FactoryInterface $factory, ContainerInterface $container,SecurityContextInterface $securityContext)
     {
         $this->factory = $factory;
         $this->container = $container;
+        $this->securityContext = $securityContext;
+        $this->isLoggedIn = $this->securityContext->isGranted('IS_AUTHENTICATED_FULLY');
+
+
     }
-    
+
     public function mainMenu()
     {
     	$menu = $this->factory->createItem('root', array(
@@ -39,6 +47,18 @@ class MenuBuilder
             'icon' => 'compass',
             'route' => 'sujet'
         ));
+        if ($this->isLoggedIn) {
+            $menu->addChild('Deconnexion', array(
+                'icon' => 'compass',
+                'route' => 'fos_user_security_logout'));
+        } else {
+            $menu->addChild('Connexion', array(
+                'icon' => 'user',
+                'route' => 'fos_user_security_login'));
+            $menu->addChild('Inscription', array(
+                'icon' => 'plus',
+                'route' => 'fos_user_registration_register'));
+        }
         return $menu;
     }
 
