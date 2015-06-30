211124e331f16fb0f383c0f9ece2a81e6a77bc9a
ziyad77 <method.z@hotmail.fr>
2015-06-30 21:03:44 +0200

    Mise en production de l'édition du profil+ Mise en production du changement de mot de passe

--- src/Stagia/UtilisateurBundle/Form/Type/ProfileFormType.php
+++ src/Stagia/UtilisateurBundle/Form/Type/ProfileFormType.php
@@ -1,24 +1,40 @@
 <?php
 
-namespace My\UserBundle\Form\Type;
+namespace Stagia\UtilisateurBundle\Form\Type;
 
 use Symfony\Component\Form\FormBuilder;
 use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class ProfileFormType extends BaseType
 {
+    protected function buildUserForm(FormBuilderInterface $builder, array $options)
+
+    {
+        $builder
+            ->add('email', 'email', array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('adresse1', null, array('label' => 'Adresse: '))
+            ->add('adresse2', null, array('label' => 'Complément d\'adresse: '))
+            ->add('codepostal', null, array('label' => 'Code Postal:'))
+            ->add('ville', null, array('label' => 'Ville:'))
+            ->add('telephone', null, array('label' => 'Téléphone:'))
+            ->add('roles', 'choice', array(
+                'choices' => array(
+                    'ROLE_MAITRE_APPRENTISSAGE' => 'Maitre d\'apprentissage',
+                    'ROLE_RESPONSABLE' => 'Résponsable',
+                    'ROLE_TUTEUR' => 'Tuteur',
+                    'ROLE_ETUDIANT' => 'Etudiant',
+                    //'ROLE_ADMIN' => 'Admin:',
+                    //'ROLE_SUPER_ADMIN' => 'Super Admin:',
+                ),
+                'multiple' => true,
+                'expanded' => true,
+                'empty_value' => false,
+            ));
+    }
 
     public function getName()
     {
-        return 'stagia_user_profile';
-    }
-
-    protected function buildUserForm(FormBuilder $builder, array $options)
-    {
-        $builder
-            ->add('email', 'email')
-            ->add('firstName')
-            ->add('lastName')
-        ;
+        return 'fos_user_profile_edit';
     }
 }
